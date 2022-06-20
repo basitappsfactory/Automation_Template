400dc23f5b71defe094f3a3264fa1624469b1367
basitappsfactory <abdul.basit@appsfactory.de>
2022-06-20 11:14:37 +0200

    Initial commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,38 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
+
+@Execution(CONCURRENT)
+public class Tests {
+
+
+    @Test
+    public void TC01() throws Exception
+    {
+        Response response = RestAssured.get("https://reqres.in/api/users/2");
+        JSONObject json = new JSONObject(response.asString());
+        JSONObject data = json.getJSONObject("data");
+        Assertions.assertTrue(data.get("id") instanceof Integer);
+        System.out.println("FirstParallelUnitTest first() start => " + Thread.currentThread().getName());
+        Thread.sleep(500);
+        System.out.println("FirstParallelUnitTest first() end => " + Thread.currentThread().getName());
+    }
+
+    
+    @Test
+    public void TC02() throws Exception
+    {
+        System.out.println("FirstParallelUnitTest second() start => " + Thread.currentThread().getName());
+        Thread.sleep(500);
+        System.out.println("FirstParallelUnitTest second() end => " + Thread.currentThread().getName());
+        Response response = RestAssured.get("https://reqres.in/api/users/2");
+        JSONObject json = new JSONObject(response.asString());
+        JSONObject data = json.getJSONObject("data");
+        Assertions.assertTrue(data.get("id") instanceof String);
+    }
+
+}
